7441735c155f4441fffbab1b974bbdb04e3cb0f0
jiang <emailtojiang@gmail.com>
2021-10-24 23:03:13 +0800

    ✅ 今日份打卡~ 2021年10月24日

---
+++ src/main/java/com/coderjiang/DailyPractice/Monthly_2021_10/LeetCode_Daily_10_24.java
@@ -0,0 +1,19 @@
+package com.coderjiang.DailyPractice.Monthly_2021_10;
+
+import java.util.List;
+
+/**
+ * <p>Creation Time: 2021-10-24 13:20:29</p>
+ * <p>Description: TODO</p>
+ *
+ * @author 太白
+ */
+public class LeetCode_Daily_10_24 {
+
+    class Solution {
+        public int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+            return 0;
+        }
+    }
+
+}
