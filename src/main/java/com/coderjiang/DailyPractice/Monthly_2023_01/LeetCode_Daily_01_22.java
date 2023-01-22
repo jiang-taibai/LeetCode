cf123584ca3ea33cbe153b5a83dcaac6f197db18
jiang <emailtojiang@gmail.com>
2023-01-22 23:26:01 +0800

    ✅ 今日份打卡~ 2023年01月22日

---
+++ src/main/java/com/coderjiang/DailyPractice/Monthly_2023_01/LeetCode_Daily_01_22.java
@@ -0,0 +1,12 @@
+package com.coderjiang.DailyPractice.Monthly_2023_01;
+
+/**
+ * <p>Creation Time: 2023-01-22 14:06:16</p>
+ * <p>Description: TODO</p>
+ *
+ * @author 太白
+ */
+public class LeetCode_Daily_01_22 {
+
+
+}
