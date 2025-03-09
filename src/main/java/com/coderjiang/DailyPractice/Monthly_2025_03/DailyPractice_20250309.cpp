4cf9b51ba1bca8ceb9bb688b8c7ff09f5ceb4ba4
Jiang Liu <emailtojiang@gmail.com>
2025-03-09 22:53:09 +0800

    ✅ 今日份打卡~

---
+++ src/main/java/com/coderjiang/DailyPractice/Monthly_2025_03/DailyPractice_20250309.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+        map<int, int> mp;
+        for(auto& item:items) mp[item[0]] = max(mp[item[0]], item[1]);
+        int maxBeauty = 0;
+        for(auto& p:mp) {
+            maxBeauty = max(maxBeauty, p.second);
+            p.second = maxBeauty;
+        }
+        vector<int> res;
+        for(auto& price:queries) {
+            auto it = mp.lower_bound(price);
+            if(it == mp.end() || it->first > price) {
+                if(it == mp.begin()) {
+                    res.push_back(0);
+                } else {
+                    it--;
+                    res.push_back(it->second);
+                }
+            } else {
+                res.push_back(it->second);
+            }
+        }
+        return res;
+    }
+};
