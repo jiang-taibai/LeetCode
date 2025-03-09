f871a663353e3c9ec2f0ad2a5a8456ec0b465d27
Jiang Liu <emailtojiang@gmail.com>
2025-03-09 23:34:53 +0800

    ✅ 今日份打卡~：改用 C++ 复习了

--- src/main/java/com/coderjiang/DailyPractice/Monthly_2025_03/DailyPractice_20250309.cpp
+++ src/main/java/com/coderjiang/DailyPractice/Monthly_2025_03/DailyPractice_20250309.cpp
@@ -1,21 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+    static vector<int> maximumBeauty(vector<vector<int> > &items, vector<int> &queries) {
         map<int, int> mp;
-        for(auto& item:items) mp[item[0]] = max(mp[item[0]], item[1]);
+        for (auto &item: items) mp[item[0]] = max(mp[item[0]], item[1]);
         int maxBeauty = 0;
-        for(auto& p:mp) {
+        for (auto &p: mp) {
             maxBeauty = max(maxBeauty, p.second);
             p.second = maxBeauty;
         }
         vector<int> res;
-        for(auto& price:queries) {
+        for (auto &price: queries) {
             auto it = mp.lower_bound(price);
-            if(it == mp.end() || it->first > price) {
-                if(it == mp.begin()) {
+            if (it == mp.end() || it->first > price) {
+                if (it == mp.begin()) {
                     res.push_back(0);
                 } else {
-                    it--;
+                    --it;
                     res.push_back(it->second);
                 }
             } else {
@@ -25,3 +28,8 @@ public:
         return res;
     }
 };
+
+int main() {
+    Solution solution;
+    return 0;
+}
