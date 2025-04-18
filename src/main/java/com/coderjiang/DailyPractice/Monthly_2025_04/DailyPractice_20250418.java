d6cfb67e9bfe84dc41994d41815e4be00a018f8f
Jiang Liu <emailtojiang@gmail.com>
2025-04-18 10:06:15 +0800

    ✅ 今日份打卡~

---
+++ src/main/java/com/coderjiang/DailyPractice/Monthly_2025_04/DailyPractice_20250418.java
@@ -0,0 +1,21 @@
+package com.coderjiang.DailyPractice.Monthly_2025_04;
+
+import java.util.*;
+
+/**
+ * @author Jiang
+ *
+ * <p>Description:</p>
+ * <p>Date: 2025-04-18 10:05:53</p>
+ */
+public class DailyPractice_20250418 {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+
+    static class Solution {
+        
+    }
+
+}
